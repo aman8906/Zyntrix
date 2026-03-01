fcfa74050d180993d634cbac087d1a2ea530c5a5
Aman Chaurasiya <ftpchaursiyaaman@gmail.com>
2026-03-01 16:48:36 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -17,6 +17,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
